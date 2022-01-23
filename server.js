93a491fcdd9f053d9a08905af9722bcecdb4efcf
Peter Jackson <peterhj@live.unc.edu>
2022-01-23 14:37:25 -0500

    Known to work, currentlt at fs module step

---
+++ server.js
@@ -0,0 +1,24 @@
+// Require http module
+const http = require('http')
+
+const port = process.env.PORT || 3000
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/html')
+  const fs = require('fs')
+
+  // Require fs module
+  fs.readFile('./www/index.html', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    console.log(data)
+  })
+  res.end(fs)
+})
+
+server.listen(port, () => {
+  console.log(`Server running at port ${port}`)
+})
