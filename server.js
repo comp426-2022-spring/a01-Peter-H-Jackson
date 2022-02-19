4aad3453a1dbcabc6cd8de246e6a7840aa2506e6
Peter Jackson <peterhj@live.unc.edu>
2022-02-19 16:30:37 -0500

    Pt2 Build

--- server.js
+++ server.js
@@ -8,12 +8,14 @@ args["port"]
 
 const port = args.port || process.env.PORT || 3000
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader("Content-Type", "text/html")
-  res.end("<h1>Hello, World!</h1>")
-})
-
-server.listen(port, ()=> {
-  console.log("Server running at port " + port)
+fs.readFile("./www/index.html", "utf8", (err, data) => {
+  const server = http.createServer((req, res) => {
+    res.statusCode = 200
+    res.setHeader("Content-Type", "text/html")
+    res.end("<h1>Hello, World!</h1>")
+  })
+  
+  server.listen(port, ()=> {
+    console.log("Server running at port " + port)
+  })
 })
